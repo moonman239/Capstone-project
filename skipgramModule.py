20fa4b2da00a9ca11ba913cf006437f809389940
moonman239 <Montana.Burr@gmail.com>
2018-09-05 22:47:40 -0600

    Local changes to skipgramModule.py

--- skipgramModule.py
+++ skipgramModule.py
@@ -1,38 +1,50 @@
 from sqlite3 import connect,OperationalError
+import time
 conn = connect("ngrams.sqlite")
 try:
-    conn.execute("CREATE TABLE skipgrams (sentence_id integer,text text,positionId integer,question boolean)")
+    conn.execute("CREATE TABLE skipgrams (sentence_id integer,text text,positionId integer,question boolean,id integer)")
 except OperationalError as e:
     print(e)
 def saveSentences(skipgramSentences,isQuestions):
     sentence_id = 0
     nextNgramId = 0
     ngramId = 0
+    insertQuery = []
+    previousTime = time.time()
+    previousI = 0
+    i = 0
+    try:
+        conn.execute("create table temp (sentence_id integer,text text,positionId integer,question boolean,id integer)")
+    except OperationalError as e:
+        print(e)
+    print("hi man")
     for sentence in skipgramSentences:
-        print(sentence)
         ngram_position_id = 0
         for ngram in sentence:
             # Store ngram in sentence.
-            ngramId = ngramId + 1
-            # If n-gram doesn't already exist in row, then insert it.
-            results = conn.execute("select count(*) from skipgrams where sentence_id=? and positionId=?",(sentence_id,ngram_position_id))
-            ngramExists = False
-            for result in results:
-                if result[0] > 0:
-                    ngramExists = True
-            #print("ngram",ngram)
-            if (ngramExists == False):
-                conn.execute("insert into skipgrams values (\'" + str(sentence_id) + "\',\'" + ngram + "\',\'" + str(ngram_position_id) + "\',\'" + str(isQuestions) + "\')")
+            insertQuery.append("insert into temp values (\'" + str(sentence_id) + "\',\'" + ngram + "\',\'" + str(ngram_position_id) + "\',\'" + str(isQuestions) + "\'," + str(ngramId) + ");")
             ngram_position_id += 1
+            ngramId = ngramId + 1
+            currentTime = time.time()
+            print(id)
+            if ((currentTime - previousTime) % 60 > 0):
+                slope = ((currentTime - previousTime) % 60) / (id - previousI)
+                print("Estimated time remaining:" + str(slope * len(skipgramSentences)))
+                previousI += 1
         sentence_id += 1
+        id += 1
+    # Insert n-grams into permanent table if they don't already exist there.
+    insertQuery = "".join(insertQuery)
+    conn.execute(insertQuery)
+    conn.execute("insert into skipgrams select t.* from temp t left join skipgrams s on t.id = s.id where s.id is null")
+    conn.execute("drop table temp")
     conn.commit()
 # Calculate skipgrams.
+# Expected output: Sentences as an array of skipgrams.
 def skipgrams(sentence):
-    # Skip-grams: ["I sell", "I seashells","I by","I seashore","I which","I are","I seashells","I by,"I seashore"]
-    sentenceSkipGramRepresentations = []
-    # Get the base word.
     splitSentence = sentence.split(" ")
     skipGrams = []
+     # Get the base word.
     for i in range(0,len(splitSentence)):
         word = splitSentence[i]
         restOfSentence = splitSentence[i + 1:len(splitSentence)]
@@ -41,7 +53,4 @@ def skipgrams(sentence):
             # Skip-grams.
             wordSkipGramCombinations.append(word + " " + word_2)
         [skipGrams.append(wordSkipGramCombination) for wordSkipGramCombination in wordSkipGramCombinations]
-    sentenceSkipGramRepresentations.append(skipGrams)
-    return sentenceSkipGramRepresentations
-
-    
+    return skipGrams
