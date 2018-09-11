da9b5b254888aec9d3bde259514b2228fa513809
moonman239 <Montana.Burr@gmail.com>
2018-09-10 19:30:30 -0600

    Moved one-hot encoding code to 'Capstone project.py ' and fixed up Capstone project.py to be compatible with this new format.

--- Capstone project.py
+++ Capstone project.py
@@ -17,65 +17,34 @@ data = json["data"]
 questions = []
 answers = []
 print("Hello")
-import string
-def isEnglish(s):
-    try:
-        s.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
-    else:
-        return True
-for article in data:
-    # Articles
-    for paragraph in article["paragraphs"]:
-        # Paragraphs
-        for qas in paragraph["qas"]:
-            # Questions/Answers
-                # Remove all punctuation and non-English characters
-                question = "".join([string for string in qas["question"] if string not in punctuation])
-                question = "".join([string for string in question if isEnglish(string)])
-                # Remove common words that probably won't affect the accuracy of the end result.
-                stop = ["The","the","A","a"]
-                for word in stop:
-                    question = question.replace(word,"")
-                answer = qas["answers"][0]["text"]
-                answer = "".join([string for string in answer if string not in punctuation])
-                if (answer != ""):
-                    questions.append(question)
-                    answers.append(answer)
 #Assert that every character of every string is an English character.
 for question in questions:
     assert isEnglish(question)
 #for answer in answers:
     #assert isEnglish(answer)
-import n_gram
-# Make sure each question has the same length - the maximum.
-maxLenQs = max([len(question) for question in questions])
-questions = [question.ljust(maxLenQs) for question in questions]
-print("maximum length of questions:" + str(maxLenQs))
-print("Number of questions:" + str(len(questions)))
-# Get the ngrams.
-print("Getting the ngrams.")
-question_ngram = n_gram.ngramStrings(questions,2)
-print("Length of question ngrams array: " + str(len(question_ngram)))
-print("Number of ngrams",len(question_ngram[0]))
-print("Finished getting the ngrams.")
-X_list = np.array(question_ngram)
-print("X list size: " + str(X_list.size))
-X_list.resize(X_list.shape[0],1,X_list.shape[1])
+print("Getting the one-hot encoded n-grams.")
+import sqlite3
+conn = sqlite3.connect("ngrams.sqlite")
+def updateFunction(y):
+    ngramVocabulary.update(y)
+conn.row_factory = lambda cursor, row: row[0]
+c = conn.cursor()
+ngrams = c.execute("select text from ngrams").fetchall()
+questionNgramRepresentations = c.execute("select text from ngrams where isQuestion=1").fetchall()
+answerNgramRepresentations = c.execute("select text from ngrams where isQuestion=0")
+import keras.preprocessing.text
+tokenizer = keras.preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(ngrams)
+questionsOneHotEncoded = tokenizer.texts_to_matrix(questionNgramRepresentations)
+answersOneHotEncoded = tokenizer.texts_to_matrix(answerNgramRepresentations)
+X_list = questionsOneHotEncoded
+Y_list = answersOneHotEncoded
+#X_list.resize(X_list.shape[0],1,X_list.shape[1])
 print("Type of X_list: " + str(X_list.dtype))
-maxLenAs = max([len(answer) for answer in answers])
-Y_list = [answer.ljust(maxLenAs) for answer in answers]
-Y_list = [answer.split(" ") for answer in Y_list]
 assertionMessage = "Assertion failed: length inequality. X_list: " + str(len(X_list)) + " Y_list " + str(len(Y_list))
 assert len(X_list) == len(Y_list),assertionMessage
 vocabulary = set()
-print("Beginning one-hot encoding.")
-from keras.preprocessing import text
-Y_list = np.array([text.one_hot(answer,len(vocabulary)) for answer in answers])
-print("Finished one-hot encoding.")
 # Expected number of dimensions: 2
-# import sklearn.preprocessing
 print("Building neural network")
 assert X_list.ndim == 3
 # Define our neural network.
@@ -89,7 +58,7 @@ model = Sequential()
 # Train our model.
 # Each X represents columns (is our X this word/that word?) 
 # Each X includes one word from the answer (or None if we're talking about the first word)
-dimensions = 100
+dimensions = 99
 print("Loaded model")
 model.add(LSTM(100,input_shape=X_list.shape[1:],return_sequences=True))
 print("X list shape: ",X_list.shape)
