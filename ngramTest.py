54f67e5e5bd7b7bfed431984d41c00aaa90f496b
moonman239 <Montana.Burr@gmail.com>
2018-09-15 15:11:46 -0600

    Added 'one-hot' encoding

--- ngramTest.py
+++ ngramTest.py
@@ -9,13 +9,16 @@ questions_as_word_sequences = list(map(keras.preprocessing.text.text_to_word_seq
 answers_as_word_sequences = list(map(keras.preprocessing.text.text_to_word_sequence,answers))
 vocabulary = column_stack((questions_as_word_sequences,answers_as_word_sequences))
 vocabulary = set(vocabulary.flatten())
+questions_as_word_sequences = []
+answers_as_word_sequences = []
 print(vocabulary)
-lenVocabulary = len(vocabulary)
+lengthVocabulary = len(vocabulary)
 def oneHot(sentence):
     return keras.preprocessing.text.one_hot(sentence,lengthVocabulary)
 skipgramFn = lambda sequence: keras.preprocessing.sequence.skipgrams(sequence,lenVocabulary)
-questions = list(map(skipgramFn,questions_as_word_sequences))
-answers = list(map(skipgramFn,answers_as_word_sequences))
+questions = list(map(oneHot,questions))
+answers = list(map(oneHot,answers))
+# How do I get questions to be just a list of numbers?
 print(questions)
 #print(answers)
 id = 0
