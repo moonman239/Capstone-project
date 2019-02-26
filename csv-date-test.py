b09ceb6745ba0bf8ae3f20a9d9161833faf7035d
moonman239 <Montana.Burr@gmail.com>
2019-02-26 10:24:35 -0700

    Fixed date testing code thanks to Stack Overflow

--- csv-date-test.py
+++ csv-date-test.py
@@ -1,13 +1,15 @@
 # Check to see why I am getting Jun-05.
 import pandas as pd
+from dateutil.parser import parse
 def dateparse(x):
     try:
+        x = "0" + x
         # Return a string.
-        date = pd.datetime.strptime(x,'%b-%y')
+        date = pd.datetime.strptime(x,'%y-%b')
         return date.strftime("%B %Y")
     except ValueError:
         return x
-print(dateparse("Jun-05")) # Test code
+print(parse("05-Jun")) # Test code
 trainData = pd.read_csv("training_data.csv",parse_dates=["answer_text"],date_parser=dateparse)
 location = trainData[trainData["answer_text"] == "June 2005"]
 print(location)
