42f7c7f13bdcc2642f12fea46fcabba4b6fdcdcf
moonman239 <Montana.Burr@gmail.com>
2019-02-26 11:11:03 -0700

    Added ability to parse multiple date formats.

--- csv-date-test.py
+++ csv-date-test.py
@@ -1,15 +1,25 @@
 # Check to see why I am getting Jun-05.
 import pandas as pd
-from dateutil.parser import parse
-def dateparse(x):
-    try:
-        x = "0" + x
-        # Return a string.
-        date = pd.datetime.strptime(x,'%y-%b')
-        return date.strftime("%B %Y")
-    except ValueError:
-        return x
-print(parse("05-Jun")) # Test code
-trainData = pd.read_csv("training_data.csv",parse_dates=["answer_text"],date_parser=dateparse)
-location = trainData[trainData["answer_text"] == "June 2005"]
+def parse(x):
+    import re
+    match_condn = r'\b([0-9])\b'
+    replace_str = r'0\1'
+    # Return a string.
+    dateFormats = ["%y-%b","%b-%y",]
+    # Iterate through all known date formats, parsing them.
+    for dateFormat in dateFormats:
+        # If date has been parsed, return. Else, keep parsing.
+        try:
+            zeroPaddedDateString = re.sub(match_condn, replace_str, x)
+            x = pd.datetime.strptime(zeroPaddedDateString,dateFormat).strftime("%B %Y")
+        except ValueError:
+            continue
+    return x
+# Test code
+import re
+print(parse("05-Jun"))
+print(parse("Apr-94"))
+print(parse("not a date"))
+#trainData = pd.read_csv("training_data.csv",parse_dates=["answer_text"],date_parser=parse)
+#location = trainData[trainData["answer_text"] == "October 2005"]
 print(location)
