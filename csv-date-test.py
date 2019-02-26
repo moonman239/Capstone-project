82d3e3b0f0b0d8a841d0f0c6032efc17c9d21b15
moonman239 <Montana.Burr@gmail.com>
2019-02-26 11:14:06 -0700

    Added a third test case - %d-%b-%y

--- csv-date-test.py
+++ csv-date-test.py
@@ -5,7 +5,7 @@ def parse(x):
     match_condn = r'\b([0-9])\b'
     replace_str = r'0\1'
     # Return a string.
-    dateFormats = ["%y-%b","%b-%y"]
+    dateFormats = ["%y-%b","%b-%y","%d-%b-%y"]
     # Iterate through all known date formats, parsing them.
     for dateFormat in dateFormats:
         # If date has been parsed, return. Else, keep parsing.
@@ -19,6 +19,7 @@ def parse(x):
 import re
 print(parse("05-Jun"))
 print(parse("Apr-94"))
+print(parse("04-Jun-05"))
 print(parse("not a date"))
 #trainData = pd.read_csv("training_data.csv",parse_dates=["answer_text"],date_parser=parse)
 #location = trainData[trainData["answer_text"] == "October 2005"]
